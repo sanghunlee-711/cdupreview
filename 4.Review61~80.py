e9dfa2a21c7016415d3173807de03e9b51859f00
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2020-06-22 20:29:27 +0900

    Algorithm 0622
    Done

--- 4.Review61~80.py
+++ 4.Review61~80.py
@@ -1,4 +1,4 @@
-"""
+
 # 61, 1064
 a, b, c = map(int, input().split())
 print(min(a,b,c))
@@ -10,8 +10,7 @@ if b % 2 == 0:
     print(b)
 if c % 2 == 0:
     print(c)
-"""
-"""
+
 #63, 1066
 a, b, c = map(int, input().split())
 if a%2 == 0:
@@ -40,7 +39,7 @@ if a %2  == 0:
     print('even')
 else:
     print('odd')
-"""
+
 #66, 1068
 
 a = int(input())
@@ -54,4 +53,47 @@ elif 69>= a >=40:
 elif 39>= a >=0:
     print("D")
 
+#67, 1069
+
+a = input()
+
+if(a == "A"):
+    print("best!!!")
+elif(a == "B"):
+    print("good!!")
+elif(a == "C"):
+    print("run!")
+elif(a == "D"):
+    print("slowly~")
+else:
+    print("what?")
     
+#68, 1070
+a = int(input())
+
+if (a == 12 or a == 1 or a == 2):
+    print("winter")
+elif(a == 3 or a ==4 or a == 5):
+    print("spring")
+elif(a == 6 or a == 7 or a == 8):
+    print("summer")
+elif(a == 9 or a == 10 or a ==11):
+    print("fall")
+
+#69, 1071
+a = list(map(int, input().split()))
+i = 0
+
+for i in range(0, len(a)-1):
+    if a[i] == 0:
+        break
+    else:
+        print(a[i]) # 안되는 이유 알아봐야할듯.
+#70, 1072
+
+
+n = int(input())
+a = list(map(int, input().split()))
+
+for i in range(0, n):
+    print(a[i])
